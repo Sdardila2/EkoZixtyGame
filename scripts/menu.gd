076f3480bf0e9975aa98e50d6944f25f8648564f
NICO10110 <ikokun1011@gmail.com>
2025-06-02 22:43:22 -0500

    menu

---
+++ scripts/menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+
+func _on_opciones_pressed():
+	get_tree().change_scene_to_file("res://scenes/opciones.tscn")
+
+
+func _on_salir_pressed():
+	get_tree().quit()
